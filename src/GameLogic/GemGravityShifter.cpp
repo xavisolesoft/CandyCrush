f3927f6e2adb6aaec98478c361e2270e4dd0d63c
Xavier Solé <xavisn1@gmail.com>
2020-03-21 17:53:11 +0100

    GemGravityShifter: remove magic numbers.

--- src/GameLogic/GemGravityShifter.cpp
+++ src/GameLogic/GemGravityShifter.cpp
@@ -7,10 +7,21 @@
 
 #include "../Geometry/Point.hpp"
 
+#include "../Animation/ComposedSequentialAnimation.hpp"
+
 using namespace GameLogic;
 using namespace GameBoard;
 using namespace Geometry;
+using namespace Animation;
 using namespace Gem;
+
+const float GemGravityShifter::BOUNCE_LENGTH = 5;
+const int GemGravityShifter::BOUNCE_STEPS = 4;
+const float GemGravityShifter::BOUNCE_PERIOD_SECONDS = 0.01f;
+
+const int GemGravityShifter::GRAVITY_STEPS = 10;
+const float GemGravityShifter::GRAVITY_PERIOD_SECONDS = 0.01f;
+const int GemGravityShifter::GRAVITY_STEP_FOR_CELL_CHANGE = 5;
 
 GemGravityShifter::GemGravityShifter()
 {
@@ -23,16 +34,31 @@ void GemGravityShifter::update(Board& gameBoard)
 			auto gem = gameBoard.getGemFromCell(i, j);
 			auto upperGem = gameBoard.getGemFromCell(i, j - 1);
 			if (!gem && upperGem && upperGem->isUserInteractionEnabled()) {
-				auto animation1 = new MoveGemAnimation();
+				auto composedAnimation = new ComposedSequentialAnimation();
+
+				auto gravityAnimation = new MoveGemAnimation();
 				Point<float> destination = gameBoard.getWorldPosFromCell(i, j);
 				Point<float> origin = gameBoard.getWorldPosFromCell(i, j - 1);
-				animation1->start(upperGem, origin, destination, 10, 0.01f);
-				upperGem->setAnimation(*animation1);
-				animation1->setStepTriggeredFunction(5,
+				gravityAnimation->start(upperGem, origin, destination, GRAVITY_STEPS, GRAVITY_PERIOD_SECONDS);
+				gravityAnimation->setStepTriggeredFunction(GRAVITY_STEP_FOR_CELL_CHANGE,
 													[&gameBoard, &gem, i, j]() {
 														gameBoard.setGemToCell(i, j, gem);
 														gameBoard.setGemToCell(i, j - 1, nullptr);
 													});
+				composedAnimation->appendAnimation(*gravityAnimation);
+
+				auto gemBottomGem = gameBoard.getGemFromCell(i, j + 1);
+				if (gemBottomGem && !gemBottomGem->getAnimation()){
+					auto bounceAnimation = new MoveGemAnimation();
+					Point<float> bounceTop(destination.getX(), destination.getY() - BOUNCE_LENGTH);
+					bounceAnimation->start(upperGem, destination, bounceTop, BOUNCE_STEPS, BOUNCE_PERIOD_SECONDS);
+					composedAnimation->appendAnimation(*bounceAnimation);
+
+					auto bounceReturnAnimation = new MoveGemAnimation();
+					bounceReturnAnimation->start(upperGem, bounceTop, destination, BOUNCE_STEPS, BOUNCE_PERIOD_SECONDS);
+					composedAnimation->appendAnimation(*bounceReturnAnimation);
+				}
+				upperGem->setAnimation(*composedAnimation);
 			}
 		}
 	}
--- src/GameLogic/GemGravityShifter.hpp
+++ src/GameLogic/GemGravityShifter.hpp
@@ -12,5 +12,14 @@ namespace GameLogic {
 		GemGravityShifter();
 
 		void update(GameBoard::Board& gameBoard);
+	
+	private:
+		static const float BOUNCE_LENGTH;
+		static const int BOUNCE_STEPS;
+		static const float BOUNCE_PERIOD_SECONDS;
+
+		static const int GRAVITY_STEPS;
+		static const float GRAVITY_PERIOD_SECONDS;
+		static const int GRAVITY_STEP_FOR_CELL_CHANGE;
 	};
 }
